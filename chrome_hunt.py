72bf8f85817872341e946f06a971017840095c4e
Colin Cowie <ccowie@purdue.edu>
2019-09-08 19:54:07 -0400

    spider with related

--- chrome_hunt.py
+++ chrome_hunt.py
@@ -41,18 +41,11 @@ class Chrome_Hunt(cmd.Cmd):
     def do_spider(self, arg):
         print("[*] Requesting chrome store content")
         # Set headers
-        headers = requests.utils.default_headers()
+        headers = requests.utils.default_headers() # Move this to global maybe?
         headers.update({ 'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'})
         response = requests.get("https://chrome.google.com/webstore/category/extensions", headers)
-
-        # Regex for URLs on homepage
-        urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', response.text)
-        extensions = []
-        for link in urls:
-            if "https://chrome.google.com/webstore/detail" in link:
-                extensions.append(link)
-        for e in extensions:
-            print("[*] Found Extension: "+e)
+        extensions = self.parse_urls(response)
+        self.parse_detail(extensions)
 
     def do_exit(self, line):
         "Exit the program"
@@ -74,6 +67,36 @@ class Chrome_Hunt(cmd.Cmd):
             zip_ref.extractall(zip[:-4])
             print("[*] Unziped extension to "+str(zip[:-4])+"/")
         print("[!] Done!")
+
+    def parse_detail(self, extensions):
+        new_extensions = []
+        for e in extensions:
+            response = requests.get(e)
+            soup = BeautifulSoup(response.text, "html.parser")
+            name = e.rsplit('/',2)[1]
+            link = e
+            # Parse the <meta> tag for the exact number, remove junk characters and round it.
+            download_tag = soup.find(itemprop="interactionCount").get("content")
+            download_count = round(float(download_tag.rsplit(':',1)[1].replace(',','').replace('+','').replace('.','')))
+            print("[*] Extension: "+name)
+            if download_count >= 50000:
+                print("\x1b[32m[+]\033[1;0m Downloads: "+str(download_count)+"")
+            else:
+                print("\033[91m[-]\033[1;0m Downloads: "+str(download_count)+"")
+            print("[*] Link: "+link+"\n[ ]")
+            # Search for releated extentions url on repsonse page. we will likely have to worry about dups later on.
+            new_extensions.append(self.parse_urls(response))
+        # Parse the new extensions!
+        self.parse_detail(extensions)
+
+    def parse_urls(self, response):
+        # Regex for URLs on homepage
+        urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', response.text)
+        extensions = []
+        for link in urls:
+            if "https://chrome.google.com/webstore/detail" in link:
+                extensions.append(link)
+        return extensions
 
 if __name__ == '__main__':
     Chrome_Hunt = Chrome_Hunt()
